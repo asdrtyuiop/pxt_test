9bd7e988fcde2546a074ab43f34d7cb4d011f687
asdrtyuiop <44797929+asdrtyuiop@users.noreply.github.com>
2019-12-27 21:30:09 +0800

    Add files via upload

--- artecrobo.ts
+++ artecrobo.ts
@@ -1,23 +1,6 @@
 /**
  * Types of DC motor control
  */
-enum DCmotion {
-	//% block="Forward1111"
-	Forward1111,
-	//% block="Backward"
-	Backward,
-	//% block="Brake"
-	Brake,
-	//% block="Coast"
-	Coast
-}
-
-enum connectorDCMotor {
-	//% block="M1"
-	P1,
-	//% block="M2"
-	P2
-}
 
 enum connectorServoMotor {
 	//% block="P13"
@@ -34,91 +17,6 @@ enum connectorServoMotor {
 //% color=#5b99a5 weight=100 icon="\uf009" block="ArtecRobo"
 namespace artecrobo {
 
-	/* spped initial value */
-	let speedM1 = 1023;
-	let speedM2 = 1023;
-	let state = DCmotion.Brake;
-
-	// Move DC motor
-	//% blockId=artec_move_dc_motor
-	//% block="DC motor %_connector| motion: %_motion"
-	export function moveDCMotor(_connector: connectorDCMotor, _motion: DCmotion): void {
-		switch(_motion) {
-			case DCmotion.Forward1111:
-				/*
-					Move Forward1111
-					M1:P8 = speed, P12 = 0
-					M2:P0 = speed, P16 = 0
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.digitalWritePin(DigitalPin.P8, 1);
-					pins.analogWritePin(AnalogPin.P12, speedM1);
-				} else {
-					pins.digitalWritePin(DigitalPin.P0, 1);
-					pins.analogWritePin(AnalogPin.P16, speedM2);
-				}
-				break;
-			case DCmotion.Backward:
-				/*
-					Move Backward
-					M1:P8 = 0, P12 = speeed
-					M2:P0 = 0, P16 = speeed
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.analogWritePin(AnalogPin.P8, speedM1);
-					pins.digitalWritePin(DigitalPin.P12, 1);
-				} else {
-					pins.analogWritePin(AnalogPin.P0, speedM2);
-					pins.digitalWritePin(DigitalPin.P16, 1);
-				}
-				break;
-			case DCmotion.Brake:
-				/*
-					Brake
-					M1:P8 = 1, P12 = 1
-					M2:P0 = 1, P16 = 1
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.digitalWritePin(DigitalPin.P8, 1);
-					pins.digitalWritePin(DigitalPin.P12, 1);
-				} else {
-					pins.digitalWritePin(DigitalPin.P0, 1);
-					pins.digitalWritePin(DigitalPin.P16, 1);
-				}
-				break;
-			case DCmotion.Coast:
-				/*
-					Coast
-					M1:P8 = 0, P12 = 0
-					M2:P0 = 0, P16 = 0
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.digitalWritePin(DigitalPin.P8, 0);
-					pins.digitalWritePin(DigitalPin.P12, 0);
-				} else {
-					pins.digitalWritePin(DigitalPin.P0, 0);
-					pins.digitalWritePin(DigitalPin.P16, 0);
-				}
-				break;
-		}
-		state = _motion;
-	}
-
-	//% blockId=artec_set_speed_dc_motor
-	//% block="DC motor %_connector| speed: %_speed"
-	//% _speed.min=0 _speed.max=1023
-	export function setSpeedDCMotor(_connector: connectorDCMotor, _speed: number): void {
-		if (_speed < 0)		{ _speed = 0; }
-		if (_speed > 1023)	{ _speed = 1023; }
-		if (_connector == connectorDCMotor.M1) {
-			speedM1 = 1023 - _speed;
-		} else {
-			speedM2 = 1023 - _speed;
-		}
-		if (state == DCmotion.Forward1111 || state == DCmotion.Backward) {
-			moveDCMotor(_connector, state);
-		}
-	}
 	let angleP13 = 90;
 	let angleP14 = 90;
 	let angleP15 = 90;
